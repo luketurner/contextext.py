39f5cbfd7c9f019cf86a1a9b3899602b4f504209
Luke Turner <github@luketurner.org>
2015-02-19 20:53:05 -0600

    Update version

--- contextext/__init__.py
+++ contextext/__init__.py
@@ -1,7 +1,7 @@
 from .api import *
 from .models import ContextEntry
 
-__version__ = "0.1.0a1"
+__version__ = "0.1.0a2"
 __author__ = "Luke Turner"
 __license__ = "MIT"
 __copyright__ = "Copyright 2015 Luke Turner"
